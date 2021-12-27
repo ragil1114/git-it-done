48a37aee6d57a7407c2541dac1bfc71a3aa8c233
Ragil14 <rahil.gilani@gmail.com>
2021-12-27 15:10:27 -0600

    read & use data from Query Parameter

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,14 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+};
 
 
 var getRepoIssues = function(repo) {
@@ -80,4 +89,4 @@ var displayWarning = function(repo) {
 };
 
   
-getRepoIssues("facebook/react");
+getRepoName();
